d10f239b628985b1a0b4c8b2e355d8bde83baf9d
Tarun Singh <TarunSingh611@protonmai.com>
2024-10-23 15:54:34 +0530

    site verification meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <Head>
+        <meta name="google-site-verification" content="SpNrpA4epU_QtBEKJfm_beI121IrI1ORz8muVem-4nY" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
         <meta name="author" content="Tarun Singh" />
