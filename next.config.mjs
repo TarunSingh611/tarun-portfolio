1bb2c8d2c1b6598cec812b67930eee7b2b700aa3
Tarun Singh <TarunSingh611@protonmail.com>
2025-08-29 23:20:38 +0530

    added image url

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['api.placeholder.com'],
+    domains: ['api.placeholder.com', 'tarunsingh611.github.io', 'tarunsinghrajput.netlify.app'],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
